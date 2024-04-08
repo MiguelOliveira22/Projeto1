2d93210927e03b58fa59726a28bbdc7b99c5171d
MiguelQueiroz <miguelqueiroz360@gmail.com>
2024-04-08 09:40:35 -0300

    Teste 1
    Adicionado CLS

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import tkinter, classes
+import os, tkinter, classes
 
 def main():
     selected = "Start"
@@ -8,12 +8,16 @@ def main():
         match selected:
             case 1:
                 func1()
+                os.system("cls") or None
             case 2:
                 func2()
+                os.system("cls") or None
             case 3:
                 func3()
+                os.system("cls") or None
             case 4:
                 func4()
+                os.system("cls") or None
 
 def options():
     print("1. Cadastro de obras de arte")
@@ -23,16 +27,16 @@ def options():
     print("0. Terminar a execução do programa")
 
 def func1():
-    pass
+    print("Pressione [ENTER] Para Continuar!")
 
 def func2():
-    pass
+    print("Pressione [ENTER] Para Continuar!")
 
 def func3():
-    pass
+    print("Pressione [ENTER] Para Continuar!")
 
 def func4():
-    pass
+    print("Pressione [ENTER] Para Continuar!")
 
 if __name__ == "__main__":
     main()  
