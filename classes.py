a9362090ddbf130c60483e4c672357ba2d4ae51c
MiguelQueiroz <miguelqueiroz360@gmail.com>
2024-04-08 09:49:16 -0300

    Re-estruturação

--- classes.py
+++ classes.py
@@ -1,14 +1,14 @@
 class Obra():
     def __init__(self):
-        self.ano_obra[4] = ""
-        self.mes_obra[2] = ""
-        self.autor_obra[20] = ""
-        self.nome_obra[20] = ""
-        self.estilo_obra[15] = ""
-        self.url_obra[100] = ""
+        self.ano_obra : str[4] = ""
+        self.mes_obra : str[2] = ""
+        self.autor_obra : str[20] = ""
+        self.nome_obra : str[20] = ""
+        self.estilo_obra : str[15] = ""
+        self.url_obra : str[100] = ""
         self.valor_estimado : float = 0
         self._aberto_para_gravação : bool = 0
-        self._arquivo = open(r"")
+        self._arquivo = 0
     
     def lerCamposDoArquivo(self):
         pass
--- main.py
+++ main.py
@@ -4,20 +4,17 @@ def main():
     selected = "Start"
     while selected != 0:
         options()
-        selected = int(input(""))
+        selected = int(input("Selecione um: "))
         match selected:
             case 1:
                 func1()
-                os.system("cls") or None
             case 2:
                 func2()
-                os.system("cls") or None
             case 3:
                 func3()
-                os.system("cls") or None
             case 4:
                 func4()
-                os.system("cls") or None
+        os.system("cls") or None
 
 def options():
     print("1. Cadastro de obras de arte")
@@ -27,16 +24,16 @@ def options():
     print("0. Terminar a execução do programa")
 
 def func1():
-    print("Pressione [ENTER] Para Continuar!")
+    input("Pressione [ENTER] Para Continuar!")
 
 def func2():
-    print("Pressione [ENTER] Para Continuar!")
+    input("Pressione [ENTER] Para Continuar!")
 
 def func3():
-    print("Pressione [ENTER] Para Continuar!")
+    input("Pressione [ENTER] Para Continuar!")
 
 def func4():
-    print("Pressione [ENTER] Para Continuar!")
+    input("Pressione [ENTER] Para Continuar!")
 
 if __name__ == "__main__":
     main()  
